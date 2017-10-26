1288459e9a1530f1d8c1ea5b2558236bc95250e1
takujimo <takujimo@localhost>
2017-10-26 13:42:10 -0400

    adding php

---
+++ Chapter01/php-app/startup-script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Modified from https://github.com/GoogleCloudPlatform/getting-started-php/blob/master/optional-compute-engine/gce/startup-script.sh
+
+# [START all]
+set -e
+export HOME=/root
+
+# [START php]
+yum update
+yum install -y httpd php php-mysql php-pdo
+systemctl start httpd.service
+systemctl enable httpd.service
+
+
+# Fetch the project ID from the Metadata server
+PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+
+# [END php]
+
+# [START project_config]
+# Fetch the application config file from the Metadata server and add it to the project
+curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/project-config" \
+  -H "Metadata-Flavor: Google" >> /opt/app/config/settings.yml
+# [END project_config]
+# [START logging]
+# Install Fluentd
+curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
+
+# Start Fluentd
+systemctl start google-fluentd.service
+# [END logging]
+# [END all]
