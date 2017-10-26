5497dc65e07128d5619ab1cb179acc9b807fc2d0
takujimo-minos <takujimo@localhost.localdomain>
2017-10-26 19:43:47 -0400

    updated startup script

--- Chapter01/php-app/startup-script.sh
+++ Chapter01/php-app/startup-script.sh
@@ -6,10 +6,8 @@ set -e
 export HOME=/root
 
 # [START php]
-yum update
-yum install -y httpd php php-mysql php-pdo
-systemctl start httpd.service
-systemctl enable httpd.service
+apt-get update
+apt-get install -y git apache2 php5 php5-mysql php5-dev php-pear pkg-config mysql-client
 
 
 # Fetch the project ID from the Metadata server
@@ -22,6 +20,9 @@ git clone https://source.developers.google.com/p/gcp-cookbook/r/gcpcookbook  /op
 cp /opt/src/Chapter01/php-app/pdo/* /var/www/html -r
 # [END php]
 
+systemctl restart apache2
+iptables -A INPUT -i eth0 -p tcp -m tcp --dport 3306 -j ACCEPT
+
 # [START project_config]
 # Fetch the application config file from the Metadata server and add it to the project
 #curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/project-config" \
@@ -29,9 +30,9 @@ cp /opt/src/Chapter01/php-app/pdo/* /var/www/html -r
 # [END project_config]
 # [START logging]
 # Install Fluentd
-curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
+sudo curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
 
 # Start Fluentd
-systemctl start google-fluentd.service
+service google-fluentd restart &
 # [END logging]
 # [END all]
